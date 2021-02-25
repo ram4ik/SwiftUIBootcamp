0f5dbf4053dd9167dc81ad386673ecda28d5c5e5
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-02-25 22:31:22 +0200

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by ramil on 25.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
